d99dbd3fe83dabd5c0236dc28346ebaffd5947c5
Ibrahim Rehman <ibrahimrehman0346@gmail.com>
2021-06-06 09:31:24 +0500

    Added Footer Section

--- src/components/HomeComponent.tsx
+++ src/components/HomeComponent.tsx
@@ -1,8 +1,11 @@
 import React, {useEffect, useState} from "react";
+import WebsiteLogo from "../assets/website-logo.png"
 import HeaderImage from "../assets/header-image.png";
 import HeaderImage2 from "../assets/header-image-2.png";
 import Button from '@material-ui/core/Button';
 import HomeWorkIcon from '@material-ui/icons/HomeWork';
+import OutlinedInput from '@material-ui/core/OutlinedInput';
+
 
 function HomeComponent(){
     const [hoverStatus1, setHoverStatus1] = useState<Boolean>(false);
@@ -20,92 +23,126 @@ function HomeComponent(){
     }, [])
 
     return(
-        <main>
-            <header className="main-header">
-                <div className="header-div">
-                    <h1>The right <span style={{color: "#8180e0", fontFamily: "Roboto"}}>decision</span> at the right time.</h1>
-                    <p>We are here to help you when you need your financial support, then we are help you.</p>
-                    <Button variant="contained">
-                        <span>
-                            Contact Us
-                        </span>
-                    </Button>
-                </div>
-                <div className="header-img">
-                    <img src={HeaderImage} alt="A Female Accountant" />
-                </div>
-            </header>
-            <article className="article-1">
-                <section className="article-1-section1">
-                    <div onMouseOver={()=>setHoverStatus1(true)} onMouseOut={()=>setHoverStatus1(false)}>
-                        <div style={{color: hoverStatus1 ? "white" : "#8180e0"}}><HomeWorkIcon style={{fontSize: "3rem", alignSelf: "center", color: 'inherit'}}/> <span style={{opacity: ".6", color: "inherit"}}>10.2%</span></div>
-                        <h3>Business Loan</h3>
-                        <p>Stay turned into the world of finance &amp; business.</p>
-                        <h6>20 months installment</h6>
+        <>
+            <main>
+                <header className="main-header">
+                    <div className="header-div">
+                        <h1>The right <span style={{color: "#8180e0", fontFamily: "Roboto"}}>decision</span> at the right time.</h1>
+                        <p>We are here to help you when you need your financial support, then we are help you.</p>
+                        <Button variant="contained">
+                            <span>
+                                Contact Us
+                            </span>
+                        </Button>
                     </div>
-                </section>
-                <section className="article-1-section2">
-                    <div onMouseOver={()=>setHoverStatus2(true)} onMouseOut={()=>setHoverStatus2(false)}>
-                        <div style={{color: hoverStatus2 ? "white" : "#8180e0"}}><HomeWorkIcon style={{fontSize: "3rem", alignSelf: "center", color: 'inherit'}}/> <span style={{opacity: ".6", color: "inherit"}}>9.35%</span></div>
-                        <h3>Educational Loan</h3>
-                        <p>Stay turned into the world of finance &amp; business.</p>
-                        <h6>20 months installment</h6>
+                    <div className="header-img">
+                        <img src={HeaderImage} alt="A Female Accountant" />
                     </div>
-                    <div style={{marginTop: "40px"}} onMouseOver={()=>setHoverStatus3(true)} onMouseOut={()=>setHoverStatus3(false)}>
-                        <div style={{color: hoverStatus3 ? "white" : "#8180e0"}}><HomeWorkIcon style={{fontSize: "3rem", alignSelf: "center", color: 'inherit'}}/> <span style={{opacity: ".6", color: "inherit"}}>28.6%</span></div>
-                        <h3>Personal Loan</h3>
-                        <p>Stay turned into the world of finance &amp; business.</p>
-                        <h6>20 months installment</h6>
+                </header>
+                <article className="article-1">
+                    <section className="article-1-section1">
+                        <div onMouseOver={()=>setHoverStatus1(true)} onMouseOut={()=>setHoverStatus1(false)}>
+                            <div style={{color: hoverStatus1 ? "white" : "#8180e0"}}><HomeWorkIcon style={{fontSize: "3rem", alignSelf: "center", color: 'inherit'}}/> <span style={{opacity: ".6", color: "inherit"}}>10.2%</span></div>
+                            <h3>Business Loan</h3>
+                            <p>Stay turned into the world of finance &amp; business.</p>
+                            <h6>20 months installment</h6>
+                        </div>
+                    </section>
+                    <section className="article-1-section2">
+                        <div onMouseOver={()=>setHoverStatus2(true)} onMouseOut={()=>setHoverStatus2(false)}>
+                            <div style={{color: hoverStatus2 ? "white" : "#8180e0"}}><HomeWorkIcon style={{fontSize: "3rem", alignSelf: "center", color: 'inherit'}}/> <span style={{opacity: ".6", color: "inherit"}}>9.35%</span></div>
+                            <h3>Educational Loan</h3>
+                            <p>Stay turned into the world of finance &amp; business.</p>
+                            <h6>20 months installment</h6>
+                        </div>
+                        <div style={{marginTop: "40px"}} onMouseOver={()=>setHoverStatus3(true)} onMouseOut={()=>setHoverStatus3(false)}>
+                            <div style={{color: hoverStatus3 ? "white" : "#8180e0"}}><HomeWorkIcon style={{fontSize: "3rem", alignSelf: "center", color: 'inherit'}}/> <span style={{opacity: ".6", color: "inherit"}}>28.6%</span></div>
+                            <h3>Personal Loan</h3>
+                            <p>Stay turned into the world of finance &amp; business.</p>
+                            <h6>20 months installment</h6>
+                        </div>
+                    </section>
+                    <section className="article-1-section3">
+                        <div className="article-1-section3-div">
+                            <h2>We provide awesome services, it's here</h2>
+                            <p>We are here to help you when you need your financial support, then we are help you.</p>
+                            <p>We are here to help you when you need your financial support, then we are help you.</p>
+                            <Button variant="contained">
+                                View More
+                            </Button>
+                        </div>
+                    </section>
+                </article>
+                <article className="main-header" style={{marginTop: "100px"}}>
+                    <div className="header-img">
+                        <img src={HeaderImage2} alt="A Female Accountant" />
                     </div>
-                </section>
-                <section className="article-1-section3">
-                    <div className="article-1-section3-div">
-                        <h2>We provide awesome services, it's here</h2>
+                    <div className="header-div">
+                        <h2>Our manager will contact you to clear the details.</h2>
                         <p>We are here to help you when you need your financial support, then we are help you.</p>
                         <p>We are here to help you when you need your financial support, then we are help you.</p>
                         <Button variant="contained">
-                            View More
+                            Call Us Now
                         </Button>
                     </div>
-                </section>
-            </article>
-            <article className="main-header" style={{marginTop: "100px"}}>
-                <div className="header-img">
-                    <img src={HeaderImage2} alt="A Female Accountant" />
-                </div>
-                <div className="header-div">
-                    <h2>Our manager will contact you to clear the details.</h2>
-                    <p>We are here to help you when you need your financial support, then we are help you.</p>
-                    <p>We are here to help you when you need your financial support, then we are help you.</p>
-                    <Button variant="contained">
-                        Call Us Now
-                    </Button>
-                </div>
-            </article>
-            <article className="article-3">
-                <div>
-                    <h2>Fast and very easy application process here</h2>
-                    <p>We are here to help you when you need your financial support, then we are help you.</p>
-                </div>
+                </article>
+                <article className="article-3">
+                    <div>
+                        <h2>Fast and very easy application process here</h2>
+                        <p>We are here to help you when you need your financial support, then we are help you.</p>
+                    </div>
+                    <section>
+                        <div onMouseOver={()=>setHoverStatus4(true)} onMouseOut={()=>setHoverStatus4(false)}>
+                            <h3 style={{color: hoverStatus4 ? "white" : "#8180e0"}}>01</h3>
+                            <h4>Apply Bank Loan</h4>
+                            <p>We are provide best services and finaancial solution for you.</p>
+                        </div>
+                        <div onMouseOver={()=>setHoverStatus5(true)} onMouseOut={()=>setHoverStatus5(false)}>
+                            <h3 style={{color: hoverStatus5 ? "white" : "#8180e0"}}>02</h3>
+                            <h4>Approved Bank Loan</h4>
+                            <p>We are provide best services and finaancial solution for you.</p>
+                        </div>
+                        <div onMouseOver={()=>setHoverStatus6(true)} onMouseOut={()=>setHoverStatus6(false)}>
+                            <h3 style={{color: hoverStatus6 ? "white" : "#8180e0"}}>03</h3>
+                            <h4>Review Your Loan</h4>
+                            <p>We are provide best services and finaancial solution for you.</p>
+                        </div>
+                    </section>
+                </article>
+            </main>
+            <footer className="footer">
                 <section>
-                    <div onMouseOver={()=>setHoverStatus4(true)} onMouseOut={()=>setHoverStatus4(false)}>
-                        <h3 style={{color: hoverStatus4 ? "white" : "#8180e0"}}>01</h3>
-                        <h4>Apply Bank Loan</h4>
-                        <p>We are provide best services and finaancial solution for you.</p>
-                    </div>
-                    <div onMouseOver={()=>setHoverStatus5(true)} onMouseOut={()=>setHoverStatus5(false)}>
-                        <h3 style={{color: hoverStatus5 ? "white" : "#8180e0"}}>02</h3>
-                        <h4>Approved Bank Loan</h4>
-                        <p>We are provide best services and finaancial solution for you.</p>
-                    </div>
-                    <div onMouseOver={()=>setHoverStatus6(true)} onMouseOut={()=>setHoverStatus6(false)}>
-                        <h3 style={{color: hoverStatus6 ? "white" : "#8180e0"}}>03</h3>
-                        <h4>Review Your Loan</h4>
-                        <p>We are provide best services and finaancial solution for you.</p>
-                    </div>
+                    <img src={WebsiteLogo} alt="Payonta" />
+                    <h6>
+                        88 694 895 684<br />
+                        88 694 895 684<br />
+                        abc@gmail.com
+                    </h6>
                 </section>
-            </article>
-        </main>
+                <section>
+                    <h6>What should you need do to get personal loan ver easay.</h6>
+                    <span>20 days ago</span>
+                    <p>Many modern alternatives often eumen incorpo other content actually detracts from...</p>
+                    <br />
+                    <br />
+                    <h6>What should you need do to get personal loan ver easay.</h6>
+                    <span>20 days ago</span>
+                    <p>Many modern alternatives often eumen incorpo other content actually detracts from...</p>
+                </section>
+                <section>
+                    <h2>Subscribe our newsletter</h2>
+                    <OutlinedInput placeholder="Email Address"/>
+                    <OutlinedInput placeholder="Phone No."/>
+                    <Button variant="contained" size="large" style={{backgroundColor: "#8180e0", color: "white"}}>
+                        Subscribe Now
+                    </Button>
+                </section>
+            </footer>
+            <section className="copyright-section">
+                <h4>Copyright <span style={{color: "#8180e0"}}>Payloan</span> All rights reserved</h4>
+            </section>
+        </>
+
     )
 }
 
